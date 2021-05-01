c5c99331c3d07ae7e776adbcfae521ea67476e52
Kiaan Castillo <kiaancastillo@gmail.com>
2021-05-01 13:29:37 -0700

    Changed arrow functions to regular functions

--- index.js
+++ index.js
@@ -7,19 +7,19 @@ const DATABASE_FILE_NAME = "database/database.json"
 
 // when the client is ready, run this code
 // this event will only trigger one time after logging in
-client.once('ready', () => {
+client.once('ready', function () {
   console.log('Ready!')
 })
 
 dotenv.config()
 client.login(process.env.TOKEN)
-client.on('message', (message) => {
+client.on('message', function (message) {
   const messageContent = message.content
   
   if (isCommandMessage(messageContent)) {
     // DELETE USER MESSAGE
     message.delete()
-      .then(msg => msg)
+      .then(function (msg) { return msg })
       .catch(console.error)
 
     // CREATE FILE
@@ -46,51 +46,61 @@ client.on('message', (message) => {
 })
 
 // Handle CREATE FILE
-const addFile = (fileName, messageObj) => fs.readFile(DATABASE_FILE_NAME, (err, data) => {
-  const json = JSON.parse(data)
-  json.files[fileName] = {
-    lines: []
-  }
-  json.active = fileName
+function addFile (fileName, messageObj) {
+  fs.readFile(DATABASE_FILE_NAME, function (err, data) {
+    const json = JSON.parse(data)
+    json.files[fileName] = {
+      lines: []
+    }
+    json.active = fileName
 
-  updateDatabase(json, messageObj)
-})
+    updateDatabase(json, messageObj)
+  })
+}
 
 // Handle ADD NEW LINE
-const addNewLine = (newLine, messageObj) => fs.readFile(DATABASE_FILE_NAME, (err, data) => {
-  const json = JSON.parse(data)
-  json.files[json.active].lines.push(newLine)
-
-  updateDatabase(json, messageObj)
-})
+function addNewLine (newLine, messageObj) {
+  fs.readFile(DATABASE_FILE_NAME, function (err, data) {
+    const json = JSON.parse(data)
+    json.files[json.active].lines.push(newLine)
+  
+    updateDatabase(json, messageObj)
+  }) 
+}
 
 // Handle EDIT LINE
-const editLine = (lineNumber, newValue, messageObj) => fs.readFile(DATABASE_FILE_NAME, (err, data) => {
-  const json = JSON.parse(data)
-  json.files[json.active].lines[parseInt(lineNumber) - 1] = newValue
+function editLine (lineNumber, newValue, messageObj) {
+  fs.readFile(DATABASE_FILE_NAME, function (err, data) {
+    const json = JSON.parse(data)
+    json.files[json.active].lines[parseInt(lineNumber) - 1] = newValue
 
-  updateDatabase(json, messageObj)
-})
+    updateDatabase(json, messageObj)
+  })
+}
 
 // Handle DELETE LINE
-const deleteLine = (lineNumber, messageObj) => fs.readFile(DATABASE_FILE_NAME, (err, data) => {
-  const json = JSON.parse(data)
-  json.files[json.active].lines.splice(parseInt(lineNumber) - 1, 1)
-
-  updateDatabase(json, messageObj)
-})
+function deleteLine (lineNumber, messageObj) {
+  fs.readFile(DATABASE_FILE_NAME, function (err, data) {
+    const json = JSON.parse(data)
+    json.files[json.active].lines.splice(parseInt(lineNumber) - 1, 1)
+  
+    updateDatabase(json, messageObj)
+  })
+}
 
 // Extract message content
-const extractContent = (messageContent, command) => messageContent.slice(command.length + 1)
+function extractContent (messageContent, command) { return messageContent.slice(command.length + 1) }
 
 // Extract command argument
-const extractCommandArgument = (messageContent, command) => messageContent.slice(command.length + 1).split("]")[0]
+function extractCommandArgument (messageContent, command) { return messageContent.slice(command.length + 1).split("]")[0] }
 
 // Extract message content with command argument
-const extractContentWithCommandArgument = (messageContent, command) => messageContent.slice(command.length + 1).split("]")[1].slice(1)
+function extractContentWithCommandArgument (messageContent, command) { 
+  return messageContent.slice(command.length + 1).split("]")[1].slice(1) 
+}
 
 // Send code to channel
-const sendCode = (fileName, lines, messageObj) => {
+function sendCode (fileName, lines, messageObj) {
   const openingClosing = "```"
   const title = `// ${fileName}`
   let lineString = ""
@@ -108,7 +118,7 @@ const sendCode = (fileName, lines, messageObj) => {
 }
 
 // Check if message starts with a command
-const isCommandMessage = (message) => {
+function isCommandMessage (message) {
   for (let command in commands) {
     if (message.startsWith(commands[command])) {
       return true
@@ -118,9 +128,10 @@ const isCommandMessage = (message) => {
 }
 
 // Update database with new JSON
-const updateDatabase = (json, messageObj) => 
-  fs.writeFile(DATABASE_FILE_NAME, JSON.stringify(json), (err, data) => {
+function updateDatabase (json, messageObj) {
+  fs.writeFile(DATABASE_FILE_NAME, JSON.stringify(json), function (err, data) {
     if (err) console.log(`Error: ${err}`)
     sendCode(json.active, json.files[json.active].lines, messageObj)
   })
+} 
 
